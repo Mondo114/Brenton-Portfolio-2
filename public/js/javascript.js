da1c40de7ce921ed6b2abd4211fa385f68a77763
Brenton Jenkins <brentonjjenkins@gmail.com>
2018-05-30 16:46:54 -0700

    added icons to navbar

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,15 +1,3 @@
-// Setting Up Handlebars
-// var source = document.getElementById("entry-template").innerHTML;
-// var template = Handlebars.compile(source);
-
-// var context = {title: "My New Post", body: "This is my first post!"};
-// var html = template(context);
-
-// var location = document.getElementById("handlebars-here").innerHTML;
-// location.appendChild(html);
-
-
-
 // Navbar Disappear and Reappear
 (function() {      
     var documentElem = $(document),
@@ -30,13 +18,13 @@
 
 // Submist Button Under Contruction Alert Message
 // onclick="submitAlert()" Add this code to sumbit button if using below code
-var submitAlert = function() {
-    event.preventDefault();
-    alert("This is under construction. Please connect with me on LinkedIn.");
-}
+// var submitAlert = function() {
+//     event.preventDefault();
+//     alert("This is under construction. Please connect with me on LinkedIn.");
+// }
 
-// Footer Home Page Functionality
-var homeScroll = function() {
-    window.scrollTo(0, 0);
-}
+// // Footer Home Page Functionality
+// var homeScroll = function() {
+//     window.scrollTo(0, 0);
+// }
 
--- server.js
+++ server.js
@@ -3,16 +3,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require('path');
-const nodemailer = require('nodemailer');
-const exphbs = require('express-handlebars');
 
 const app = express();
 
 var PORT = process.env.PORT || 3000;
-
-// View engine setup
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
 
 // Static folder
 app.use('/public', express.static(path.join(__dirname, 'public')));
@@ -24,58 +18,6 @@ app.use(bodyParser.raw({type: "application/vnd.custom-type"}));
 app.use(bodyParser.text({type: "text/html"}));
 
 app.use("/", express.static("./"));
-
-app.get('/', (req, res) => {
-  res.render('main');
-});
-
-app.post('/send', (req, res) => {
-  const output = `
-    <p>You have a new message from your portfolio website.</p>
-    <h3>Contact Details</h3>
-    <ul>  
-      <li>Full Name: ${req.body.fullname}</li>
-      <li>Email: ${req.body.email}</li>
-    </ul>
-    <h3>Message</h3>
-    <p>${req.body.message}</p>
-  `;
-
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    // host: 'mail.brentonjjenkins.com',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    service: "gmail",
-    auth: {
-      user: 'bobcheeseman15@gmail.com', // generated ethereal user
-      pass: 'socheesy15:(' // generated ethereal password
-    },
-    tls:{
-      rejectUnauthorized:false
-}
-  });
-
-  // setup email data with unicode symbols
-  let mailOptions = {
-    from: '"Brenton Portfolio Website Contact" <brentonjjenkins@gmail.com>', // sender address
-    to: 'brentonjjenkins@gmail.com', // list of receivers
-    subject: 'Brenton Portfolio Website Contact Message', // Subject line
-    text: 'Hello World', // plain text body
-    html: output // html body
-  };
-
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return console.log(error);
-    }
-    console.log('Message sent: %s', info.messageId);
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-    res.render('main', {msg:'Your email has been sent'});
-  });
-});
 
 app.listen(PORT, function () {
   console.log("Portfolio is listening on PORT: " + PORT);
